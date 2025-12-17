af42c3ec26a758e8812371e767931ddc75904546
Amin-Tamang <amin100369@gmail.com>
2025-12-17 16:07:30 +0545

    first commit test

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "JournalApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "JournalApp.Pages")]
